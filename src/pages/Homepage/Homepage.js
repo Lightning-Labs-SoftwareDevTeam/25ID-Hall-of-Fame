afba756eba0666429152d80066842445bc13fd2c
RickBrennan1990 <134807616+RickBrennan1990@users.noreply.github.com>
2024-11-13 09:30:51 -1000

    added button kinda

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,49 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import inducteeService from '../../services/inducteeService';
+import React, { useState, useEffect } from 'react'
+import inducteeService from '../../services/inducteeService'
 
-function Homepage() {
-    const [inductees, setInductees] = useState(null);
+function Homepage () {
+  const [inductees, setInductees] = useState(null)
 
-    useEffect(() => {
-        const getInductees = async () => {
-            try{
-                let updatedInductees = await inducteeService.getInductees();
-                setInductees(updatedInductees);
-            } catch (error) {
-                console.error(error);
-            }
-        }
+  useEffect(() => {
+    const getInductees = async () => {
+      try {
+        let updatedInductees = await inducteeService.getInductees()
+        setInductees(updatedInductees)
+      } catch (error) {
+        console.error(error)
+      }
+    }
 
-        getInductees()
-    }, [])
+    getInductees()
+  }, [])
 
-    return (
-        <div>
-            <header>
-                Welcome to 25ID Hall of Fame!
-            </header>
+  return (
+    <div>
+      <header>Welcome to 25ID Hall of Fame!</header>
 
-            {inductees && inductees.length > 0 ? (
-                <ul>
-                    {inductees.map((inductee) => (
-                        <li key={inductee.id}>
-                            <h3>{inductee.name}</h3>
-                            <p>{inductee.citation}</p>
-                            {inductee.image && (
-                                <img 
-                                    src={`data:image/jpeg;base64,${inductee.image}`} 
-                                    alt={`${inductee.name}'s image`} 
-                                    style={{ width: '100px', height: '100px' }}
-                                />
-                            )}
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No inductees available.</p>
-            )}
-        </div>
-    )
+      {inductees && inductees.length > 0 ? (
+        <ul>
+          {inductees.map(inductee => (
+            <li key={inductee.id}>
+              <button onClick={() => handleClick(inductee)}>
+                <h3>
+                  {inductee.rank} {inductee.name} ({inductee.unit})
+                </h3>
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No inductees available.</p>
+      )}
+    </div>
+  )
 }
 
-export default Homepage;
+export default Homepage
